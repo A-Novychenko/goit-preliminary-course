27977c0104b38bccf7921fb2572670ea9e16b7de
Aleksandr Novychenko <100874103+A-Novychenko@users.noreply.github.com>
2022-10-19 18:11:34 +0300

    1

--- js/main.js
+++ js/main.js
@@ -26,3 +26,38 @@
 // console.log(result1);
 // const result2 = 108 + 223 - 2 / 5;
 // console.log(result2);
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Example 4 - Класс Math Напиши скрипт, который выводит в консоль округленные вверх/вниз и т.д.
+// значения переменной value. Используй методы Math.floor(), Math.ceil() и Math.round(). Проверь что
+// будет в консоли при значениях 27.3 и 27.9.
+
+const value = 5.5;
+let res1 = Math.floor(value);
+// 5;
+let res2 = Math.ceil(value);
+// 6;
+let res3 = Math.round(value);
+// 6;
+
+console.log(value);
+console.log(res1);
+console.log(res2);
+console.log(res3);
+
+let width = '50.5px';
+const width1 = Number(width);
+const width2 = Number.parseInt(width);
+const width3 = Number.parseFloat(width);
+console.log(width);
+console.log(typeof width);
+console.log(width1);
+console.log(typeof width1);
+console.log(width2);
+console.log(typeof width2);
+console.log(width3);
+console.log(typeof width3);
+
+const result = Number.isNaN(width2);
+console.log(result);
