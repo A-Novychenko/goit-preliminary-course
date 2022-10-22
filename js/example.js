b2a6296fdf494ac33394421203372d101390f73e
Aleksandr Novychenko <100874103+A-Novychenko@users.noreply.github.com>
2022-10-23 01:39:16 +0300

    m1 l1 e10 completion l1

--- js/example.js
+++ js/example.js
@@ -149,4 +149,38 @@
 // Example 10 - Опертор % и методы строк Напиши скрипт который переведёт значение
 // totalMinutes (количество минут) в строку в формате часов и минут HH:MM.
 
-// 70 покажет 01:10 450 покажет 07:30 1441 покажет 24:01 const totalMinutes = 70;
+// 70 покажет 01:10 450 покажет 07:30 1441 покажет 24:01
+
+//1.перевести минуты в часы
+//--вычеслить часы
+//--вычеслить минуты
+//--2.создать переменную время в формате чч:мм
+//3.вывести в консоль время
+
+// const totalMinutes = 1441;
+// const totalHours = Math.floor(totalMinutes / 60);
+// let totalHoursMinutes = totalMinutes % 60;
+// let zero;
+
+// if (totalHours >= 0 && totalHours <= 9) {
+//   zero = 0;
+// } else {
+//   zero = '';
+// }
+
+// if (totalHoursMinutes < 10) {
+//   totalHoursMinutes = '0' + totalHoursMinutes;
+// }
+
+// const time = `Время ${zero}${totalHours}:${totalHoursMinutes}`;
+// console.log(time);
+
+// //решение из урока
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
+
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
