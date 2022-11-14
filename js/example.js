7d14dc9930da89709c18a741590d989fdd092e41
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-11-14 20:12:54 +0200

    Update example.js

--- js/example.js
+++ js/example.js
@@ -1341,3 +1341,25 @@
 // cartAutoParts.addNewProduct({ name: 'monro', price: 1800 });
 
 // console.table(cartAutoParts.getproductList());
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//************   МОДУЛЬ 3 ЗАНЯТТЯ 6 */
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Example 1
+//
+// // - Деструктуризація Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
+// незалежних аргументів.
+
+// function calcBMI(weight, height) { const numericWeight = Number(weight.replace(',', '.')); const
+// numericHeight = Number(height.replace(',', '.')); return Number((numericWeight / numericHeight \*\*
+// 2).toFixed(1)); }
+
+// // Було // console.log(calcBMI('88,3', '1.75')); // console.log(calcBMI('68,3', '1.65')); //
+// console.log(calcBMI('118,3', '1.95'));
+
+// // Очікується console.log( calcBMI({ weight: '88,3', height: '1.75', }), ); console.log( calcBMI({
+// weight: '68,3', height: '1.65', }), ); console.log( calcBMI({ weight: '118,3', height: '1.95', }),
+// );
