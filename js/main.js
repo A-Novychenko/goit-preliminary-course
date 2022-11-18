6099ccfbf372ff5c22f1ac90617ecc31ca8b64b2
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-11-18 16:04:29 +0200

    test sort()

--- js/main.js
+++ js/main.js
@@ -1758,3 +1758,41 @@
 // console.log(y);
 // console.log(z);
 // console.log(g);
+
+// const letters = ['b', 'B', 'a', 'A', 'c', 'C'];
+// letters.sort();
+// console.log(letters); // ['A', 'B', 'C', 'a', 'b', 'c']
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = books
+//   .filter(book => book.rating > MIN_BOOK_RATING)
+//   .map(book => book.author)
+//   .sort((firstAuthor, secondAuthor) => firstAuthor.localeCompare(secondAuthor));
+// // const names = books.map(book => book.author).filter(el => el > MIN_BOOK_RATING);
+// // //   .sort((firstBook, secondBook) => firstBook.name.localeCompare(secondBook.name));
+// console.log(names);
