6abab6fc8ae706a5e1eaa4e697595ab4333f0648
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-11-30 19:38:29 +0200

    makeProdCard

--- js/testDOM1.js
+++ js/testDOM1.js
@@ -16,11 +16,32 @@ import products from './data/proucts.js';
 }
 
 /*
- * Пишем функцию для создания карточки продукта
+ * Пишем функцию для создания карточки продукта(объект в папке data)
  */
 
-const cardSectionRef = document.createElement('article');
+const makeProdCard = products => {
+  return products.map(product => {
+    const cardSectionRef = document.createElement('article');
+    cardSectionRef.classList.add('product__name');
 
-const cardTitleRef = document.createElement('h2');
-const cardDescrRef = document.createElement('p');
-const cardPriseRef = document.createElement('p');
+    const cardTitleRef = document.createElement('h2');
+    cardTitleRef.classList.add('product__name');
+    cardTitleRef.textContent = product.name;
+
+    const cardDescrRef = document.createElement('p');
+    cardDescrRef.classList.add('product__descr');
+    cardDescrRef.textContent = product.description;
+
+    const cardPriseRef = document.createElement('p');
+    cardPriseRef.classList.add('product__pridct');
+    cardPriseRef.textContent = `Цена: ${product.price} кредитов`;
+
+    cardSectionRef.append(cardTitleRef, cardDescrRef, cardPriseRef);
+    return cardSectionRef;
+  });
+};
+
+const arrprodCard = makeProdCard(products);
+
+const prodCardRef = document.querySelector('.js-prod-card');
+prodCardRef.append(...arrprodCard);
