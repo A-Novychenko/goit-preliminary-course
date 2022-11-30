fca5704b8d837be1a18220b707f59886af59a943
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-11-30 19:50:38 +0200

    Update testDOM1.js

--- js/testDOM1.js
+++ js/testDOM1.js
@@ -19,29 +19,54 @@ import products from './data/proucts.js';
  * Пишем функцию для создания карточки продукта(объект в папке data)
  */
 
-const makeProdCard = products => {
-  return products.map(product => {
-    const cardSectionRef = document.createElement('article');
-    cardSectionRef.classList.add('product__name');
+// const makeProdCard = products => {
+//   return products.map(({ name, description, price }) => {
+//     const cardSectionRef = document.createElement('article');
+//     cardSectionRef.classList.add('product__name');
 
-    const cardTitleRef = document.createElement('h2');
-    cardTitleRef.classList.add('product__name');
-    cardTitleRef.textContent = product.name;
+//     const cardTitleRef = document.createElement('h2');
+//     cardTitleRef.classList.add('product__name');
+//     cardTitleRef.textContent = name;
 
-    const cardDescrRef = document.createElement('p');
-    cardDescrRef.classList.add('product__descr');
-    cardDescrRef.textContent = product.description;
+//     const cardDescrRef = document.createElement('p');
+//     cardDescrRef.classList.add('product__descr');
+//     cardDescrRef.textContent = description;
 
-    const cardPriseRef = document.createElement('p');
-    cardPriseRef.classList.add('product__pridct');
-    cardPriseRef.textContent = `Цена: ${product.price} кредитов`;
+//     const cardPriseRef = document.createElement('p');
+//     cardPriseRef.classList.add('product__pridct');
+//     cardPriseRef.textContent = `Цена: ${price} кредитов`;
 
-    cardSectionRef.append(cardTitleRef, cardDescrRef, cardPriseRef);
-    return cardSectionRef;
-  });
+//     cardSectionRef.append(cardTitleRef, cardDescrRef, cardPriseRef);
+//     return cardSectionRef;
+//   });
+// };
+
+// const arrprodCard = makeProdCard(products);
+
+// const prodCardRef = document.querySelector('.js-prod-card');
+// prodCardRef.append(...arrprodCard);
+
+const makeProdCard = ({ name, description, price }) => {
+  const cardSectionRef = document.createElement('article');
+  cardSectionRef.classList.add('product__name');
+
+  const cardTitleRef = document.createElement('h2');
+  cardTitleRef.classList.add('product__name');
+  cardTitleRef.textContent = name;
+
+  const cardDescrRef = document.createElement('p');
+  cardDescrRef.classList.add('product__descr');
+  cardDescrRef.textContent = description;
+
+  const cardPriseRef = document.createElement('p');
+  cardPriseRef.classList.add('product__pridct');
+  cardPriseRef.textContent = `Цена: ${price} кредитов`;
+
+  cardSectionRef.append(cardTitleRef, cardDescrRef, cardPriseRef);
+  return cardSectionRef;
 };
 
-const arrprodCard = makeProdCard(products);
+const arrprodCard = products.map(makeProdCard);
 
 const prodCardRef = document.querySelector('.js-prod-card');
 prodCardRef.append(...arrprodCard);
