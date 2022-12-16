2bd908cc3750c673b208298d1454609fab1f3600
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-16 17:30:16 +0200

    q

--- js/feedback.js
+++ js/feedback.js
@@ -1,41 +1,65 @@
-const STORAGE_KEY = 'feedback-msg';
+const STORAGE_KEY = 'saveFormData';
 
 const formData = {};
 
 const refs = {
   form: document.querySelector('.js-feedback-form'),
+  input: document.querySelector('.js-input-name'),
   textarea: document.querySelector('.js-feedback-form-textarea'),
 };
 
 refs.form.addEventListener('click', onFormSubmit);
-refs.textarea.addEventListener('input', _.throttle(onTextareaInput, 1000));
-form.addEventListener('input', onFormInput);
+// refs.textarea.addEventListener('input', _.throttle(onTextareaInput, 1000));
+refs.form.addEventListener('input', onFormInput);
 
-populateTextarea();
+populate();
 
 function onFormSubmit(e) {
   if (e.target.classList.contains('js-btn-submit')) {
     e.preventDefault();
-    console.log('Submit form');
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
-
-    localStorage.setItem('saveFormData', JSON.stringify(formData));
   }
 }
 
-function onTextareaInput(e) {
-  const message = e.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
-}
+// function onTextareaInput(e) {
+//   // const message = e.target.value;
+//   // localStorage.setItem(STORAGE_KEY, message);
+// }
 
 function onFormInput(e) {
-  formData[e.target.name] = e.taget.value;
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  if (savedMessage) {
-    refs.textarea.value = savedMessage;
+// function populateInput() {
+//   const savedName = localStorage.getItem(STORAGE_KEY);
+//   const nameObj = JSON.parse(savedName);
+//   if (nameObj) {
+//     refs.input.value = nameObj.name;
+//   }
+// }
+function populate() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  console.log('savedData', savedData);
+  const dataObj = JSON.parse(savedData);
+  console.log('dataObj', dataObj);
+  console.log(dataObj);
+
+  // {name: 'adsfvwfvrtdbstredafbvwredav', message: 'wefvwrevwdavwrvervwt'}
+  // {message: 'wefvwrevwdavwrvervwt'}
+  // {name: 'adsfvwfvrtdbstredafbvwredav'}
+  // {}
+
+  if (dataObj) {
+    if (dataObj.name) {
+      refs.input.value = dataObj.name;
+    }
+  }
+
+  if (dataObj) {
+    if (dataObj.message) {
+      refs.textarea.value = dataObj.message;
+    }
   }
 }
