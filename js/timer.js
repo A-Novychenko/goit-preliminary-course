721d01cb9c65775f2b2e1bc1b0438a69c317a4bd
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-18 17:05:26 +0200

    upd timer

--- js/timer.js
+++ js/timer.js
@@ -6,87 +6,104 @@ const refreshBtn = document.querySelector('.js-refresh');
 const form = document.querySelector('.js-form-time');
 const inputForm = document.querySelector('.js-input-time');
 const beginBtn = document.querySelector('.js-begin');
+const label = document.querySelector('.js-start-time-label');
 
 let startTime = 1;
 let timerId = null;
 
 form.addEventListener('submit', onFormSubmit);
+btn.addEventListener('click', onBtnClik);
 
 function onFormSubmit(e) {
   e.preventDefault();
   startTime = Number(e.target.elements.startTime.value);
-  start();
+  timeValue.textContent = startTime;
   e.currentTarget.reset();
+  start();
+
   beginBtn.setAttribute('disabled', 'disabled');
   inputForm.setAttribute('disabled', 'disabled');
   stopBtn.removeAttribute('disabled');
   stopBtn.classList.toggle('timer-start-disabled');
   refreshBtn.removeAttribute('disabled');
   refreshBtn.classList.toggle('timer-start-disabled');
-}
 
-function start() {
-  timerId = setInterval(() => {
-    startTime -= 1;
-    timeValue.textContent = startTime;
-    if (!startTime) {
-      stop();
-      beginBtn.removeAttribute('disabled');
-    }
-  }, 1000);
+  beginBtn.classList.toggle('btn-hidden');
+  continueBtn.classList.toggle('btn-hidden');
+  timeValue.classList.toggle('btn-hidden');
+  inputForm.classList.toggle('btn-hidden');
+  label.classList.toggle('label-hidden');
 }
-
-function stop() {
-  clearInterval(timerId);
-}
-
-function refresh() {
-  clearInterval(timerId);
-}
-
-btn.addEventListener('click', onBtnClik);
 
 function onBtnClik(e) {
   if (e.target.classList.contains('js-continue')) {
-    continueBtn.setAttribute('disabled', 'enabled');
-    continueBtn.classList.toggle('timer-start-disabled');
-
-    stopBtn.removeAttribute('disabled');
-    stopBtn.classList.toggle('timer-start-disabled');
-
+    countdownСontinues();
     start();
     return;
   }
 
   if (e.target.classList.contains('js-stop')) {
-    continueBtn.removeAttribute('disabled');
-    continueBtn.classList.toggle('timer-start-disabled');
-
-    stopBtn.setAttribute('disabled', 'enabled');
-    stopBtn.classList.toggle('timer-start-disabled');
-
     stop();
     return;
   }
 
   if (e.target.classList.contains('js-refresh')) {
-    const isRunning = continueBtn.classList.contains('timer-start-disabled');
-
-    refreshBtn.setAttribute('disabled', 'disabled');
-    refreshBtn.classList.toggle('timer-start-disabled');
-    beginBtn.removeAttribute('disabled');
-    inputForm.removeAttribute('disabled');
-
-    if (isRunning) {
-      stopBtn.classList.toggle('timer-start-disabled');
-      stopBtn.setAttribute('disabled', 'enabled');
-    } else {
-      continueBtn.classList.toggle('timer-start-disabled');
-      continueBtn.setAttribute('disabled', 'enabled');
-    }
-
-    timeValue.textContent = 0;
     refresh();
     return;
   }
+}
+
+function start() {
+  timerId = setInterval(() => {
+    startTime -= 1;
+
+    timeValue.textContent = startTime;
+    if (!startTime) {
+      // alert('Час вийшов!!!');
+      stop();
+      refresh();
+    }
+  }, 1000);
+}
+
+function countdownСontinues() {
+  continueBtn.setAttribute('disabled', 'enabled');
+  continueBtn.classList.toggle('timer-start-disabled');
+
+  stopBtn.removeAttribute('disabled');
+  stopBtn.classList.toggle('timer-start-disabled');
+}
+
+function stop() {
+  clearInterval(timerId);
+  continueBtn.removeAttribute('disabled');
+  continueBtn.classList.toggle('timer-start-disabled');
+
+  stopBtn.setAttribute('disabled', 'enabled');
+  stopBtn.classList.toggle('timer-start-disabled');
+}
+
+function refresh() {
+  clearInterval(timerId);
+  const isRunning = continueBtn.classList.contains('timer-start-disabled');
+
+  refreshBtn.setAttribute('disabled', 'disabled');
+  refreshBtn.classList.toggle('timer-start-disabled');
+  beginBtn.removeAttribute('disabled');
+  inputForm.removeAttribute('disabled');
+  beginBtn.classList.toggle('btn-hidden');
+  continueBtn.classList.toggle('btn-hidden');
+  timeValue.classList.toggle('btn-hidden');
+  inputForm.classList.toggle('btn-hidden');
+  label.classList.toggle('label-hidden');
+
+  if (isRunning) {
+    stopBtn.classList.toggle('timer-start-disabled');
+    stopBtn.setAttribute('disabled', 'enabled');
+  } else {
+    continueBtn.classList.toggle('timer-start-disabled');
+    continueBtn.setAttribute('disabled', 'enabled');
+  }
+
+  // timeValue.textContent = 0;
 }
